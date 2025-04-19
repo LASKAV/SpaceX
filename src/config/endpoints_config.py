ee8fb0094ef6fa91e490eded5842f48b2f20365c
Mykhailo Laskavyi <mksidoops@gmail.com>
2025-04-19 22:32:41 +0200

    Add endpoints configuration for SpaceX API

---
+++ src/config/endpoints_config.py
@@ -0,0 +1,8 @@
+ENDPOINTS = {
+    "ships": "https://api.spacexdata.com/v4/ships",
+    "rockets": "https://api.spacexdata.com/v4/rockets",
+    "payloads": "https://api.spacexdata.com/v4/payloads",
+    "launchpads": "https://api.spacexdata.com/v4/launchpads",
+    "launches": "https://api.spacexdata.com/v4/launches",
+    "cores": "https://api.spacexdata.com/v4/cores"
+}
