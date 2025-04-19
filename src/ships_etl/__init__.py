12a27cd3bc0a40bfa519f7d8919ee4725c63584c
Mykhailo Laskavyi <mksidoops@gmail.com>
2025-04-19 22:32:32 +0200

    Add __init__.py to define package exports for ships ETL module

---
+++ src/ships_etl/__init__.py
@@ -0,0 +1,6 @@
+from .ships_extract import get_ships_data
+from .ships_transform import transform_ships_data
+from .ships_load import load_data_to_csv
+
+
+__all__ = ["get_ships_data", "transform_ships_data", "load_data_to_csv"]
